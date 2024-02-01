5e7f786f727037195e40f3cd7bede8164a857460
Franco <freconur@gmail.com>
2024-02-01 17:52:48 -0500

    fix: corrigiendo estilos del header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,8 +3,8 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-        {/* <div className="parallelogram"></div> */}
-        {/* <div className="polka"></div> */}
+      {/* <div className="parallelogram"></div> */}
+      {/* <div className="polka"></div> */}
       <div className="photo_perfil_container">
         <img
           className="photo_perfil_image"
@@ -15,9 +15,11 @@ const Header = () => {
       <div className="perfil_info_container">
         <h2>Hola, soy Franco y soy programador <span className="sub">Fullstack javascript</span></h2>
         <p>
-          Soy programador Fullstack javascript, en mi experiencia vengo
-          desarrollando proyectos personales con las tecnologias mas demandantes, estoy muy motivado con el
-          desarrollo de aplicaciones web, y sigo aprendiendo todos los dias.
+          Soy desarrollador fronted, tengo conocimiento solidos del lenguaje de
+          programación de javascript, typescript con experiencia en Reactjs y React
+          Native y he venido desarrollando proyectos fronted, backend y aplicaciones
+          mobile con react native. Me gustaria trabajar en proyectos con React y siempre
+          dispuesto a aprender nuevas tecnologias.
         </p>
         <div className="button_container">
           <a href="#contactme">
